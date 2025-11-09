657e2c7b35b25484c0f906cacfd68ad0dc2340bb
boneyphilip <boneyphilip@ymail.com>
2025-11-09 19:24:27 +0100

    Added frontend post submission form

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+# posts/views.py
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
+# Home page view
 def home(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'posts/home.html', {'posts': posts})
+    # Get all posts from the database (latest first)
+    posts = Post.objects.all().order_by('-id')
+
+    # If user submitted form
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save post to DB
+            return redirect('/')  # reload home page
+    else:
+        form = PostForm()  # empty form
+
+    # This line must be inside the function (indented)
+    return render(request, 'posts/home.html', {'posts': posts, 'form': form})
