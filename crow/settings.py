202b51db94126ed1603a325b7629cc197373402e
boneyphilip <boneyphilip@ymail.com>
2025-11-15 01:13:28 +0100

    Made post time display friendlier (like ‘2 hours ago’ instead of full date)

--- crow/settings.py
+++ crow/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',    # our new app added
+    'django.contrib.humanize',  # enables natural time filters
 ]
 
 MIDDLEWARE = [
